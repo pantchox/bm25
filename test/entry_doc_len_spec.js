190cf68c910d4c5727d4439c816790d7fc780435
John Jung <zjohn77@gmail.com>
2017-08-16 20:06:50 -0400

    cleaned up test dir

--- test/entry_doc_len_spec.js
+++
@@ -1,5 +0,0 @@
-/*DOCSTRING: test doc_len.js on a 2d-array (matrix) of terms.*/
-var docLen = require("../app/view/doc_len/doc_len.js");
-
-
-console.log(docLen(sampMatr));// docLen(sampMatr) = [57.1%, 143%]
